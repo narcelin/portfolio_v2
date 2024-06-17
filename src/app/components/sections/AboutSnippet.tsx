38acc4a0fcdca26f7653545408e421beeb15ebc1
Nicolas Arcelin <nicolasarcelin@Nicolass-MacBook-Pro.local>
2024-06-17 17:27:30 -0400

    changed about me

--- src/app/components/sections/AboutSnippet.tsx
+++ src/app/components/sections/AboutSnippet.tsx
@@ -18,24 +18,31 @@ export default function AboutSnippet() {
       <div className="w-3/4 xl:w-full flex flex-col  gap-10">
         <MyHeader headerType={2}>Digital Journey</MyHeader>
         <div className="text-shadow text-xl leading-">
-          I started off as a civil engineer with a dual degree in engineering
-          from Florida Gulf Coast University. Over the years of working
-          different project management positions in a variety of construction
-          companies, I found my true passion in software.
+          I began my career as a civil engineer, earning a dual degree in
+          engineering from Florida Gulf Coast University. Over the years, I held
+          various project management positions across different construction
+          companies. During this time, I discovered my true passion for software
+          development.
         </div>
         <div className="text-shadow text-xl">
-          I always found myself creating my own VBA scripts on excel, fixing our
-          websites issues, and would became the guy to go to for technical
-          expertise in my office. Intrigued by the idea of making my work more
-          effecient, I decided to expand my knowlege in software.
+          I frequently created VBA scripts in Excel, fixed website issues, and
+          became the go-to person for technical expertise in my office. Driven
+          by a desire to enhance efficiency, I decided to delve deeper into
+          software development.
         </div>
         <div className="text-shadow text-xl">
-          At first, it started off with learning the basics. The typical
-          &quot;Hello, World.&quot; applications with HTML, CSS, and Javascript.
-          Eventually it progressed to using frameworks like ReactJS and NextJS
-          with TailwindCSS. Throughout this journey I&apos;ve become more adapt
-          at using tools like VSCode and understanding the importance of proper,
-          simple code.
+          My journey began with learning the basics, starting with simple
+          &quot;Hello, World&quot; applications in HTML, CSS, and JavaScript. As
+          I honed my skills, I progressed to using frameworks like ReactJS and
+          NextJS, along with TailwindCSS. Along the way, I became proficient
+          with tools like VSCode and developed a strong appreciation for clean,
+          efficient code.
+        </div>
+        <div className="text-shadow text-xl">
+          This transition from civil engineering to software development has
+          allowed me to merge my technical background with my newfound passion,
+          opening up new avenues for creativity and problem-solving in my
+          career.
         </div>
         <div className="flex flex-row justify-center gap-10">
           <MyButton
