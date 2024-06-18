9943a6c0cfa7433048133de0e71f442161f6474a
Nicolas Arcelin <nicolasarcelin@Nicolass-MacBook-Pro.local>
2024-06-18 08:52:03 -0400

    light theme default, need to fix dark theme

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -8,7 +8,7 @@ import { ThemeProvider } from "next-themes";
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
         {children}
         <ToastContainer />
       </ThemeProvider>
