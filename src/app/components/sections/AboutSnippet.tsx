ab7935ccbde4c5d12dba526217eff036c98dbc87
Nicolas Arcelin <narcelin@Nicolass-MacBook-Pro.local>
2024-11-19 12:39:19 -0500

    Civil oriented

--- src/app/components/sections/AboutSnippet.tsx
+++ src/app/components/sections/AboutSnippet.tsx
@@ -17,7 +17,7 @@ export default function AboutSnippet() {
       />
       <div className="w-3/4 xl:w-full flex flex-col  gap-10">
         <MyHeader headerType={2}>Digital Journey</MyHeader>
-        <div className="text-shadow text-xl leading-">
+        {/* <div className="text-shadow text-xl leading-">
           I began my career as a civil engineer, earning a dual degree in
           engineering from Florida Gulf Coast University. Over the years, I held
           various project management positions across different construction
@@ -43,6 +43,28 @@ export default function AboutSnippet() {
           allowed me to merge my technical background with my newfound passion,
           opening up new avenues for creativity and problem-solving in my
           career.
+        </div> */}
+        <div className="text-shadow text-xl">
+          I began my career as a civil engineer, earning a dual degree in
+          engineering from Florida Gulf Coast University. Over the years, I held
+          various project management positions across different construction
+          companies, where I developed a strong technical foundation and a knack
+          for problem-solving.
+        </div>
+        <div className="text-shadow text-xl">
+          In these roles, I leveraged technology to improve project workflows,
+          creating VBA scripts in Excel to streamline data analysis and
+          developing solutions for website issues to enhance communication. My
+          expertise in software tools like HTML, CSS, JavaScript, and frameworks
+          such as ReactJS and TailwindCSS allowed me to implement efficient,
+          customized solutions tailored to construction management challenges.
+        </div>
+        <div className="text-shadow text-xl">
+          This blend of civil engineering expertise and technical proficiency
+          has enabled me to optimize processes, improve collaboration, and add
+          measurable value to construction projects. I am eager to bring this
+          integrated skill set to a forward-thinking team, driving innovation
+          and efficiency in the construction industry.
         </div>
         <div className="flex flex-row justify-center gap-10">
           <MyButton
