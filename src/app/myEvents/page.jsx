b2d909d9fdc8458f0768781a95b36a0201086b05
Nicolas Arcelin <nico@Nicolass-MacBook-Pro.local>
2025-03-01 04:52:05 -0800

    myEvents added

---
+++ src/app/myEvents/page.jsx
@@ -0,0 +1,19 @@
+import ReachMe from "../components/sections/ReachMe";
+import ContactMe from "../components/sections/ContactMe";
+
+import PageHeading from "../components/PageHeading";
+
+export default function Contact() {
+  return (
+    <main className="flex flex-col items-center py-14 gap-10">
+      <PageHeading
+        heading="MY EVENTS"
+        subHeading="Contact me directly at arcelin.nicolas@gmail.com"
+        // heading="Contact"
+        // subHeading="Let's Connect and Create Something Amazing Together: Reach Out to Start a Conversation About Your Project or Idea Today!"
+      />
+      {/* <ContactMe /> */}
+      {/* <ReachMe className="container" /> */}
+    </main>
+  );
+}
