1ae27aec27bf18a26aa3d99016e6b88c58a5093f
Nicolas Arcelin <nico@Mac.hitronhub.home>
2025-03-05 07:02:23 -0800

    added suppression warning in html for console error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning={true}>
       <body
         className={`${inter_Tight.variable} p-4 font-sans md:w-4/5 mx-auto`}
       >
