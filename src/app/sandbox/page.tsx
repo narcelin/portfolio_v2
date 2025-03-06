7b95a06504ea7b7719b30bd59dae8021a9857ae4
Nicolas Arcelin <nico@Mac.hitronhub.home>
2025-03-05 20:13:20 -0800

    supabase connected and working

--- src/app/sandbox/page.tsx
+++ src/app/sandbox/page.tsx
@@ -1,16 +1,19 @@
 "use client";
 
 import MyButton from "../components/MyButton";
+import { supabase } from "../utils/database/supabase";
 
 export default function sandbox() {
+  const testSupabase = async () => {
+    const client = supabase;
+    const { data, error } = await supabase.from("sandbox").select("*");
+    console.log(data);
+  };
+
   return (
     <div className="w-full h-screen flex flex-row justify-center items-center">
-      <MyButton
-        onClick={() => console.log("Clicked")}
-        fancy={false}
-        withArrow={false}
-      >
-        Education
+      <MyButton onClick={() => testSupabase()} fancy={false} withArrow={false}>
+        Test Supabase
       </MyButton>
     </div>
   );
