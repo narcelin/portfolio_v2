0cf9ab113f4a19e849a4ddad5869acb2138157c8
Nicolas Arcelin <nico@Mac.hitronhub.home>
2025-03-08 09:33:25 -0800

    removing usetheme from sandbox

--- src/app/sandbox/page.tsx
+++ src/app/sandbox/page.tsx
@@ -1,17 +1,11 @@
 "use client";
 
 import { supabase } from "../utils/database/supabase";
-import { useTheme } from "next-themes";
 
 import MyButton from "../components/MyButton";
 import MyThemeSwitch from "../components/MyThemeSwitch";
 
 export default function sandbox() {
-  const { resolvedTheme, setTheme, theme, systemTheme, forcedTheme } =
-    useTheme();
-  // setTheme("dark");
-  // console.log("THEME: ", forcedTheme);
-  // console.log("SYSTEM THEME: ", systemTheme);
   const testSupabase = async () => {
     const client = supabase;
     const { data, error } = await supabase.from("sandbox").select("*");
@@ -21,9 +15,6 @@ export default function sandbox() {
   return (
     <div className="w-full h-screen flex flex-row justify-center items-center">
       <MyThemeSwitch />
-      {/* <MyButton onClick={() => testSupabase()} fancy={false} withArrow={false}>
-        Test Supabase
-      </MyButton> */}
     </div>
   );
 }
